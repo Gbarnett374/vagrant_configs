06164108fac2dd406d465fc819758a1b9faccd47
gbarnett374 <gbarnett374@gmail.com>
2016-02-07 20:52:22 -0500

    Added some packages for apt-get

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,9 @@ sudo apt-get install -y -q \
   build-essential \
   git \
   rake \
-  tree
+  tree \
+  nodejs \
+  libgmp-dev 
 
 # install ruby using rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
