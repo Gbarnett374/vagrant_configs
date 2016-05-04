fa132241ae5246ba518fa8d5bc71f512e6ae73d3
gbarnett374 <gbarnett374@gmail.com>
2016-05-04 17:39:47 -0400

    Bunch of changes

--- Vagrantfile
+++ Vagrantfile
@@ -3,12 +3,15 @@ Vagrant.configure("2") do |config|
   config.vm.provision :shell, :path => "bootstrap.sh", privileged: false
 
   #homefolder
-  config.vm.synced_folder "/Users/gbarnett", "/home/vagrant/gbarnett"
-
+  config.vm.synced_folder "/Users/gregory.barnett", "/home/vagrant/gregory.barnett"
+  config.vm.provider :virtualbox do |vb|
+  # Use VBoxManage to customize the VM. For example to change memory:
+  vb.customize ["modifyvm", :id, "--memory", "2048", "--cpus", "2"]
+end
 # copy keys
-  config.vm.provision "file", source: "~/.ssh/id_rsa", destination: "/home/vagrant/.ssh/id_rsa"
-  config.vm.provision "shell", inline: "chmod 0400 /home/vagrant/.ssh/id_rsa"
+  # config.vm.provision "file", source: "~/.ssh/id_rsa", destination: "/home/vagrant/.ssh/id_rsa"
+  # config.vm.provision "shell", inline: "chmod 0400 /home/vagrant/.ssh/id_rsa"
 
 #forward port for webrick. 
-  config.vm.network "forwarded_port", guest: 3000, host: 3000
+  # config.vm.network "forwarded_port", guest: 3000, host: 3000
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,23 @@ sudo apt-get install -y -q \
   rake \
   tree \
   nodejs \
-  libgmp-dev 
+  libgmp-dev \
+  postgresql \
+  postgresql-contrib \
+  automake \
+  libqtwebkit-dev \
+  gstreamer1.0-plugins-base \
+  gstreamer1.0-tools \
+  gstreamer1.0-x \
+  docbook \
+  docbook-xsl \
+  libtool \
+  libjpeg-dev \
+  libpq-dev \
+  redis-server
+
+
+
 
 # install ruby using rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
@@ -17,9 +33,29 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 # creates the basic directory
 mkdir -p /home/vagrant/www
 
+
+# RVM STUFF
+
+#Load RVM into a shell session *as a function*
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+
+  # First try to load from a user install
+  source "$HOME/.rvm/scripts/rvm"
+
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+
+ # Then try to load from a root install
+  source "/usr/local/rvm/scripts/rvm"
+
+else
+
+  printf "ERROR: An RVM installation was not found.\n"
+
+fi
+
 #install latest version of ruby and set as the default.
-rvm install ruby 2.3.0
-rvm --default use ruby 2.3.0
+rvm install ruby 2.3.1
+rvm --default use ruby 2.3.1
 
 #install bundler.
 gem install bundler
