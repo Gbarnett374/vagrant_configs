e546151062dd4f9243487ccb870a76d7f076d351
gbarnett374 <gbarnett374@gmail.com>
2016-05-11 06:39:52 -0400

    Updates, added apt-get update/upgrade, env vars for home folder location, and bash code to load rvm

--- Vagrantfile
+++ Vagrantfile
@@ -3,8 +3,8 @@ Vagrant.configure("2") do |config|
   config.vm.provision :shell, :path => "bootstrap.sh", privileged: false
 
   #homefolder
-  config.vm.synced_folder "/Users/gbarnett", "/home/vagrant/gbarnett"
-
+  config.vm.synced_folder ENV['HOME'], "/home/vagrant/#{ENV['USER']}"
+  
 # copy keys
   config.vm.provision "file", source: "~/.ssh/id_rsa", destination: "/home/vagrant/.ssh/id_rsa"
   config.vm.provision "shell", inline: "chmod 0400 /home/vagrant/.ssh/id_rsa"
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 sudo apt-get remove ruby
 
 # install essential apps
+sudo apt-get update -y -q
+sudo apt-get upgrade -y -q
 sudo apt-get install -y -q \
   build-essential \
   git \
@@ -16,6 +18,24 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 
 # creates the basic directory
 mkdir -p /home/vagrant/www
+
+
+#Load RVM into a shell session *as a function*
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+
+  # First try to load from a user install
+  source "$HOME/.rvm/scripts/rvm"
+
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+
+ # Then try to load from a root install
+  source "/usr/local/rvm/scripts/rvm"
+
+else
+
+  printf "ERROR: An RVM installation was not found.\n"
+
+fi
 
 #install latest version of ruby and set as the default.
 rvm install ruby 2.3.0
